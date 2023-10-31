d92d8d4bd62792faefefdb49745fd1224b16cd1a
unknown <marymarie905@gmail.com>
2023-10-31 21:32:58 +0200

    second project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Products from './components/products/Products.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
 import Home from './components/home/Home.jsx'
 import Restaurant from './components/restaurant/Restaurant.jsx'
+import PageNotFound from './components/pageNotFound/PageNotFound.jsx'
 import { Routes, Route} from 'react-router-dom'
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Products />} />
         <Route path='/restaurant' element={<Restaurant />} />
+        <Route path='*' element={<PageNotFound />} />
         
 
       </Routes>
